3a6814942cd74c89dccc7fddf1540b96488ba236
Michal K Kucharski <mkuchar@cs.washington.edulocal>
2018-05-22 23:56:35 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ess-library',
+    version='0.1.1',
+    description='ESS Classes to create pipeline',
+    packages=find_packages()
+)
